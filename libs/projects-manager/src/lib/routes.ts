93b3317d9f2ff1bf0fdbd002e1afac629e766396
Adam Wolak <adamwolak0501@gmail.com>
2024-07-18 07:49:41 +0200

    Add Simple libs.

---
+++ libs/projects-manager/src/lib/routes.ts
@@ -0,0 +1,13 @@
+import { Route } from '@angular/router';
+import { ProjectListComponent } from './pages/project-list/project-list.component';
+
+export const appRoutes: Route[] = [
+ {
+   path: 'list',
+   component: ProjectListComponent
+ },
+ {
+   path: 'create',
+   component: ProjectListComponent
+ },
+]
